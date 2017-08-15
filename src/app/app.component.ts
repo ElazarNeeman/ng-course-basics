6623711cf44c7152129409a467bccbb20578227c
elazar neeman <elazar@xwiserd.com>
2017-08-15 09:51:27 +0300

    Adding todolist module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{title}}</h1>
+    <!--
+    <input  #inputeText [placeholder]="text">
+    <button (click)="changeTitle(inputeText.value)">Click me</button>
+    -->
+    <app-todolist></app-todolist>
+  `,
 })
 export class AppComponent {
-  title = 'app';
+
+  public title: string;
+  public text: string;
+
+  constructor() {
+   this.title = 'Hello angular 123';
+   //
+   // setTimeout( () => { this.title = 'zzz'; }, 1000);
+  }
+
+  // public changeTitle(newValue: string): void {
+  //   this.title = newValue;
+  //   // this.text = 'choose title';
+  // }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {TestCompComponent} from './app.test-comp.component';
+import {TodolistModule} from './todolist/todolist.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestCompComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TodolistModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
